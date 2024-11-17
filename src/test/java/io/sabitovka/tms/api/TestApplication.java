fe1376b0c27c9a5e547f33cd37338ffc88b0f0c7
Karim Sabitov <sabitov@scioffice.com>
2024-11-17 11:30:05 +0300

    Initial commit

---
+++ src/test/java/io/sabitovka/tms/api/TestApplication.java
@@ -0,0 +1,11 @@
+package io.sabitovka.tms.api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
